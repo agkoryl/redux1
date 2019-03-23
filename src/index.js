250e7d60b76743a2c8e87af50b3a36c24f73dd1e
Agnieszka Koryl <ag.koryl@gmail.com>
2019-03-23 09:52:22 +0100

    redux extension

--- src/index.js
+++ src/index.js
@@ -4,28 +4,31 @@ import './index.css';
 import App from './App';
 
 
-import {createStore} from 'redux';
+import { createStore } from 'redux';
 
 const INCREMENT_COUNTER = 'INCREMENT_COUNTER';
 const DECREMENT_COUNTER = 'DECREMENT_COUNTER';
 
 const counterReducer = (state = 0, action) => {
 
-    switch(action.type) {
+    switch (action.type) {
         case INCREMENT_COUNTER:
-        return state + 1
+            return state + 1
         case DECREMENT_COUNTER:
-        return state - 1
+            return state - 1
         default:
-        return state
+            return state
     }
 }
 
-let store = createStore(counterReducer);
+let store = createStore(counterReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
 
-store.dispatch({type: INCREMENT_COUNTER})
-store.dispatch({type: DECREMENT_COUNTER})
-store.dispatch({type: DECREMENT_COUNTER})
+store.dispatch({ type: INCREMENT_COUNTER })
+store.dispatch({ type: DECREMENT_COUNTER })
+store.dispatch({ type: DECREMENT_COUNTER })
 
 console.log(store.getState());
 
