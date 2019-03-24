9210d11368b6d3b1cefd9152c4014a970306ff9e
Agnieszka Koryl <ag.koryl@gmail.com>
2019-03-24 10:49:20 +0100

    to do list redux

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Counter from './Counter/Counter';
+import TodoList from './Todo/Todo';
 
 
 
@@ -9,8 +10,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-    REDUX COUNTER
+    <h2>REDUX COUNTER</h2>
     <Counter></Counter>
+    <div style={{display: "flex", justifyContent: "center"}}>
+    <TodoList></TodoList>
+    </div>
       </div>
     );
   }
---
+++ src/Todo/Todo.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import './Todo.css';
+import { removeToDo, newToDo } from '../store/todos';
+
+const TodoList = props => {
+
+    const handleKeyPress = event => {
+        if(event.which === 13) {
+            const value = event.target.value;
+            props._addtask(value);
+          }
+    }
+
+    
+    return (
+        <div className="table-container">
+            <h2>TO DO REDUX LIST</h2>
+            <input onKeyPress={handleKeyPress}/>
+            <table className="comicGreen todo-table">
+            <tbody>
+            {
+                    props._todos
+                        ? props._todos.map(todo => {
+                            return <tr key={todo.id}>
+                                <td>{todo.text}</td>
+                                <td><button type="button" onClick={() => props._removetask(todo.id)}>X</button></td>
+                            </tr>
+
+                        })
+                        : <div>No todos on your list yet</div>
+                }
+            </tbody> 
+            </table>
+        </div>
+    )
+}
+
+const mapStateToProps = state => ({
+    _todos: state.toDos
+})
+
+const mapDispatchToProps = dispatch => ({
+    _addtask: (toDoValue)=> dispatch(newToDo(toDoValue)),
+    _removetask: (id) => dispatch(removeToDo(id))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
