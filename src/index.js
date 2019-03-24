0d8c7aacfcf095f7c553013d0a0df0a7cb46c46d
Agnieszka Koryl <ag.koryl@gmail.com>
2019-03-24 08:30:38 +0100

    react-redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers } from 'redux';
+import {Provider} from 'react-redux';
 
 import './index.css';
 import App from './App';
@@ -30,6 +31,10 @@ window.remove = (id) => store.dispatch(removeToDo(id))
 console.log(store.getState());
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>, document.getElementById('root')
+);
 
 
